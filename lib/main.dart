bbad18b31a9a675fdd9d1e2593b58968786be45b
AbdoulDaff <abdoulniallydaff@gmail.com>
2022-07-13 17:13:35 +0000

    add All

--- lib/main.dart
+++ lib/main.dart
@@ -33,8 +33,6 @@ class MyApp extends StatelessWidget {
                 Container(
                     color: Colors.green,
                     width: 100,
-
-
                     height: 100
                 ),
               ],
