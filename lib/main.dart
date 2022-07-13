a082e1153d26088d98d9470349e4ac10b9793af8
AbdoulDaff <abdoulniallydaff@gmail.com>
2022-07-13 16:54:55 +0000

    add

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Layout Challenge',
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              color: Colors.red,
+              width: 100,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                    color: Colors.yellow,
+                    width: 100,
+                    height: 100
+                ),
+                Container(
+                    color: Colors.green,
+                    width: 100,
+                    height: 100
+                ),
+              ],
+            ),
+          Container(
+            color: Colors.blue,
+            width: 100,
+
+        ),
+          ],
+        ),
+      ),
+    );
+  }
+}
